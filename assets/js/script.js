bc184a295df6af82e17aacfc9612b0fd9e24c119
Toky <tokyrakotoarivony5gmail.com>
2022-09-11 21:26:15 +0300

    responsive nav-link

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,24 @@ links.forEach((link) => {
     link.classList.add("active");
   });
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLink = document.querySelector(".nav-link");
+const cloneBody = document.querySelector(".clone-body");
+const main = document.querySelector("main");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLink.classList.toggle("active");
+  cloneBody.classList.toggle("active");
+  main.classList.toggle("z1");
+});
+
+cloneBody.addEventListener("click", () => {
+  if (cloneBody.classList.value.includes("active")) {
+    hamburger.classList.remove("active");
+    navLink.classList.remove("active");
+    cloneBody.classList.remove("active");
+    main.classList.remove("z1");
+  }
+});
